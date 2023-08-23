a65e7654666c032c40e87d742eddb3141ae5c371
Rafael <rafaelalpes39@gmail.com>
2023-08-23 17:17:37 +0800

    Send Mail

---
+++ app/Mail/OrderShipped.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class OrderShipped extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+
+    public $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    public function build()
+    {
+
+        /*  return $this->view('emails.orders.shipped')
+            ->with(['data' => $this->data]); */
+        return $this->subject('New Contact Form Submission')
+            ->from($this->data['email'], $this->data['name'])
+            ->markdown('emails.orders.shipped')
+            ->with(['data' => $this->data]);
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Order Shipped',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            markdown: 'emails.orders.shipped',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
